6b3b224a1ecc0531880727bf668e464b48c10048
Indre Bakeer <indre.bakeer@gmail.com>
2019-08-29 02:52:41 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -13,5 +13,14 @@ class RecipesController < ApplicationController
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+    redirect_to recipe_path(@recipe)
   end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, ingredients_attributes: [:name, :quantity])
+  end
+  
 end
